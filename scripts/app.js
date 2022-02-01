b11b87520eda1dcfd454cdd2d340023eee2a4f4b
Brian Miranda <brianmiranda.dev@gmail.com>
2022-02-01 00:42:43 -0500

    Foundations
    Adding the skeleton and all the scripts

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+// INITIALIZING FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyD61MQ18ibGkA_wsxB_KG2Lc5Jpp0qZY3Q",
+  authDomain: "wishingto.firebaseapp.com",
+  projectId: "wishingto",
+  storageBucket: "wishingto.appspot.com",
+  messagingSenderId: "994257848607",
+  appId: "1:994257848607:web:52501d863d9c934465c82a",
+  measurementId: "G-K8KJ559BXZ",
+};
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+// APP
